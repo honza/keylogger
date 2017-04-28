28b2d07e269272aba198d6be81182d7057108c81
Honza Pokorny <me@honza.ca>
2017-04-28 15:06:23 -0300

    Bump version

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
-(defproject keylogger "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+(defproject keylogger "0.1"
+  :description "keylogger analyzer"
   :url "http://example.com/FIXME"
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [cheshire "5.7.1"]]
